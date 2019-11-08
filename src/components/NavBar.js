f17b9f0254d97c0be054b38dbacf76fa23147cbc
Jonas Jensen <jonas.jensen@msn.com>
2019-11-08 23:38:02 +0100

    integrate with auth0, add basic navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar as Nav, Alignment } from "@blueprintjs/core"
+import Profile from "./Profile";
+
+const NavBar = () =>
+  <Nav>
+    <Nav.Group>
+      <Nav.Heading>Fooddecider</Nav.Heading>
+    </Nav.Group>
+    <Nav.Group align={Alignment.RIGHT}>
+      <Profile/>
+    </Nav.Group>
+  </Nav>;
+
+export default NavBar;
---
+++ src/components/Profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Menu, MenuItem, Popover, Position } from "@blueprintjs/core";
+import { useAuth0 } from "../react-auth0-spa";
+
+const Profile = () => {
+  const { loading, user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  if (!isAuthenticated) {
+    return <Button rightIcon='log-in' text='Log in' onClick={loginWithRedirect} />;
+  }
+
+  return (
+    <Popover
+      content={(<Menu><MenuItem text="Log out" icon="log-out" onClick={logout} /></Menu>)}
+      position={Position.BOTTOM_LEFT}
+    >
+      <Button
+        className='bp3-minimal'
+        rightIcon='caret-down'
+        text={user ? user.name : ''}
+        loading={loading || !user}
+      />
+    </Popover>
+  );
+};
+
+export default Profile;
