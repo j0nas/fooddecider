82ff81d885d43098db4286b0cd383055469561e8
Jonas Jensen <jonas.jensen@msn.com>
2019-12-01 18:04:08 +0100

    set up react-router-dom routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import NavBar from './components/NavBar';
 import { CompanyAssociationSelection } from './views/CompanyAssociationSelection';
 import { RestaurantAssociationSelection } from './views/RestaurantAssociationSelection';
-
 const App = () => (
-  <div className="bp3-dark">
-    <NavBar />
-    <CompanyAssociationSelection />
-    <RestaurantAssociationSelection />
-  </div>
+  <BrowserRouter>
+    <div className="bp3-dark">
+      <NavBar />
+      <Switch>
+        <Route path="/company">
+          <CompanyAssociationSelection />
+        </Route>
+        <Route path="/restaurant">
+          <RestaurantAssociationSelection />
+        </Route>
+        <Route>
+          <Redirect to="/restaurant" />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
