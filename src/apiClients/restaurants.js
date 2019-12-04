0e6a093d3610d61c0f907920d95667e435286190
Jonas Jensen <jonas.jensen@msn.com>
2019-12-04 23:21:12 +0100

    fetch restaurants using employee's association attribute

--- src/apiClients/restaurants.js
+++ src/apiClients/restaurants.js
@@ -2,10 +2,21 @@ import { addRestaurant, endpoint as employeesEndpoint } from './employees';
 
 export const endpoint = '/restaurants';
 
-export const fetchRestaurants = () => fetch(endpoint).then(res => res.json());
+export const fetchRestaurants = async () => {
+  const response = fetch(endpoint);
+  return await response.json();
+};
 
-export const fetchRestaurantsForUser = userId =>
-  fetch(`${employeesEndpoint}/${userId}${endpoint}`).then(res => res.json());
+export const fetchRestaurant = async id => {
+  const response = await fetch(`${endpoint}/${id}`);
+  return await response.json();
+};
+
+export const fetchRestaurantsForUser = async userId => {
+  const response = await fetch(`${employeesEndpoint}/${userId}`);
+  const { restaurants } = await response.json();
+  return await Promise.all(restaurants.map(fetchRestaurant));
+};
 
 export const createRestaurant = async (name, userId) => {
   const response = await fetch(endpoint, {
